9ab264db26d2b323ba6377ebeaf92cb970f4a384
Yufan Liu <Yufan-l@users.noreply.github.com>
2018-03-19 20:14:36 -0700

    Add replace trigger table logic

--- hydra/src/main/java/com/github/codegerm/hydra/trigger/record/NewRecordTrigger.java
+++ hydra/src/main/java/com/github/codegerm/hydra/trigger/record/NewRecordTrigger.java
@@ -1,11 +1,18 @@
 package com.github.codegerm.hydra.trigger.record;
 
+import com.github.codegerm.hydra.utils.AvroSchemaUtils;
+
 public class NewRecordTrigger extends SqlResultTrigger {
 
 	private static final String SQL_SELECT_COUNT = "select count(*) from ";
 
 	@Override
 	protected String generateSQL(String parameters) {
+        //replace the schema
+		String replace = AvroSchemaUtils.getReplaceTriggerTable(context);
+		if(replace!=null){
+			parameters = AvroSchemaUtils.replaceTableNameOfTableByEnv(parameters, replace);
+		}
 		return SQL_SELECT_COUNT + parameters;
 	}
 
--- hydra/src/main/java/com/github/codegerm/hydra/utils/AvroSchemaUtils.java
+++ hydra/src/main/java/com/github/codegerm/hydra/utils/AvroSchemaUtils.java
@@ -8,22 +8,26 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import org.apache.avro.Schema;
+import org.apache.flume.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.github.codegerm.hydra.source.SqlSourceUtil;
 import com.google.common.base.Strings;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
 public final class AvroSchemaUtils {
 
 	private static final Logger logger = LoggerFactory.getLogger(AvroSchemaUtils.class);
-
+	private static final Gson GSON = new GsonBuilder().create();
+	
 	public static List<Schema> getSchemasAsList(String schemas) {
 		if (Strings.isNullOrEmpty(schemas)) {
 			return null;
@@ -67,13 +71,14 @@ public final class AvroSchemaUtils {
 	/*
 	 *  { "schema1.table1" : "schema2.table1", "table1" : "table2" }
 	 */
+	
 	public static Map<String, String> replaceTableNameByEnv(Map<String, String> schemaMap, String mapString) {
 		
 		try{
 			if(mapString != null && !mapString.isEmpty()){
 				logger.info("Replace table in env found: " + mapString);
 				Type mapType = new TypeToken<Map<String, String>>(){}.getType();  
-				Map<String, String> map = new Gson().fromJson(mapString, mapType);
+				Map<String, String> map = GSON.fromJson(mapString, mapType);
 				Map<String, String> nodeMap = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
 				nodeMap.putAll(map);
 				Map<String, String> newSchemaMap = new HashMap<String, String>();
@@ -106,7 +111,7 @@ public final class AvroSchemaUtils {
 			if(mapString != null && !mapString.isEmpty()){
 				logger.info("Replace schema in env found: " + mapString);
 				Type mapType = new TypeToken<Map<String, String>>(){}.getType();  
-				Map<String, String> map = new Gson().fromJson(mapString, mapType);
+				Map<String, String> map = GSON.fromJson(mapString, mapType);
 				Map<String, String> nodeMap = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
 				nodeMap.putAll(map);
 				Map<String, String> newSchemaMap = new HashMap<String, String>();
@@ -137,5 +142,92 @@ public final class AvroSchemaUtils {
 		return schemaMap;
 
 	}
+	
+	@Deprecated
+	public static String replaceSchemaNameOfTableByEnv(String tableName, String mapString) {
+		try{
+			if(mapString != null && !mapString.isEmpty()){
+				Type mapType = new TypeToken<Map<String, String>>(){}.getType();  
+				Map<String, String> map = GSON.fromJson(mapString, mapType);
+				Map<String, String> nodeMap = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
+				nodeMap.putAll(map);
+				String content[] = tableName.split("\\.");
+				 if(content.length<2){
+					 logger.warn("Table name: "+tableName+" has no schema name, skipping");
+					 return tableName;
+				 } else {
+					 if(nodeMap.containsKey(content[0])){
+						 String newName = nodeMap.get(content[0]);
+						 for(int i = 1;i<content.length;i++){
+							 newName = newName+"."+content[i];
+						 }
+						 logger.info("Replace table: " + tableName +" to " + newName);						
+						 return tableName;
+					 }
+				 }
+				
+			}
+		} catch (Exception e){
+			logger.error("Table name replace failed, keep orginal name", e);
+		}
+		return tableName;
+
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static String getReplaceSchemas(Context context) {
+		String userParamsStr = context.getString("userParams");
+		if (userParamsStr != null) {
+			try {
+				Map<String, String> userParams = GSON.fromJson(userParamsStr, Map.class);
+				if (userParams != null) {
+					return userParams.get(SqlSourceUtil.SCHEMA_NAME_REPLACE_ENV);
+				}
+			} catch (JsonSyntaxException e) {
+				logger.warn("Can't parse userParams", e);
+			}
+		}
+		return null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static String getReplaceTriggerTable(Context context) {
+		String userParamsStr = context.getString("userParams");
+		if (userParamsStr != null) {
+			try {
+				Map<String, String> userParams = GSON.fromJson(userParamsStr, Map.class);
+				if (userParams != null) {
+					return userParams.get(SqlSourceUtil.TRIGGER_TABLE_NAME_REPLACE_ENV);
+				}
+			} catch (JsonSyntaxException e) {
+				logger.warn("Can't parse userParams", e);
+			}
+		}
+		return null;
+	}
+	
+	public static String replaceTableNameOfTableByEnv(String tableName, String mapString) {
+		try{
+			if(mapString != null && !mapString.isEmpty()){
+				Type mapType = new TypeToken<Map<String, String>>(){}.getType();  
+				Map<String, String> map = GSON.fromJson(mapString, mapType);
+				Map<String, String> nodeMap = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
+				nodeMap.putAll(map);
+
+				if(nodeMap.containsKey(tableName)){
+					String newName = nodeMap.get(tableName);
+					logger.info("Replace table: " + tableName +" to " + newName);
+					return newName;
+				}
+
+
+			}
+		} catch (Exception e){
+			logger.error("Table name replace failed, keep orginal name", e);
+		}
+		return tableName;
+
+	}
+
 
 }
